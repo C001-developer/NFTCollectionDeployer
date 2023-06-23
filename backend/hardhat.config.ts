1d4b2787e70ee9ab4f238dd26768975d436a35e5
Cool Developer <cool.kevin.dev@outlook.com>
2023-06-23 07:48:15 -0400

    add the public testnet

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.1",
@@ -19,6 +22,11 @@ module.exports = {
     localnet: {
       chainId: 31337,
       url: "http://127.0.0.1:8545",
+    },
+    goerli: {
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 5,
     },
     // Other network configurations...
   },
--- backend/server/EventFilter.ts
+++ backend/server/EventFilter.ts
@@ -1,10 +1,10 @@
-import  { ethers } from "hardhat";
+import { ethers, config } from "hardhat";
 import dotenv from "dotenv";
+import { HttpNetworkConfig } from "hardhat/types";
 
 dotenv.config();
 
 const nftFactoryAddress = process.env.NFT_FACTORY_ADDRESS || "";
-const rpcEndpoint = process.env.RPC_ENDPOINT || "";
 
 const nftCollectionAddress: string[] = [];
 const nftCollectionEvents: any[] = [];
@@ -15,50 +15,58 @@ export async function syncEvents() {
     if (nftFactoryAddress === "") {
         throw new Error("NFT_FACTORY_ADDRESS is not set");
     }
-    if (rpcEndpoint === "") {
-        throw new Error("RPC_ENDPOINT is not set");
-    }
 
-    const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+    const provider = new ethers.providers.JsonRpcProvider((config.networks["goerli"] as HttpNetworkConfig).url); // you can use any network here, like `localnet` or `goerli`
 
     const nftFactory = await ethers.getContractAt("NFTCollectionFactory", nftFactoryAddress, provider.getSigner());
 
     const filter = nftFactory.filters.CollectionCreated();
 
-    
 
-    let lastBlockNumber = 1;
+
+    let lastBlockNumber = Number(process.env.DEPLOYED_BLOCK_NUMBER) || 1;
+    let syncFlag = false;
 
     setInterval(async () => {
-        const blockNumber = await provider.getBlockNumber();
-        if (lastBlockNumber > blockNumber) {
-            return;
+        try {
+            if (syncFlag) {
+                return;
+            }
+            syncFlag = true;
+            const blockNumber = await provider.getBlockNumber();
+            if (lastBlockNumber >= blockNumber) {
+                syncFlag = false;
+                return;
+            }
+            const events = await nftFactory.queryFilter(filter, lastBlockNumber, blockNumber - 1);
+
+            for (const event of events) {
+                nftCollectionEvents.push(event);
+
+                const collectionAddress = event.args?.[0];
+                nftCollectionAddress.push(collectionAddress);
+
+                const collection = await ethers.getContractAt("NFTCollection", collectionAddress, provider.getSigner());
+                const mintFilter = collection.filters.TokenMinted();
+
+                nftMintFilters.push({ "contract": collection, "filter": mintFilter });
+            }
+
+            for (const mintFilter of nftMintFilters) {
+                const mintEvents = await mintFilter.contract.queryFilter(mintFilter.filter, lastBlockNumber, blockNumber - 1);
+                nftMintEvents.push(...mintEvents);
+                if (mintEvents.length > 0) {
+                    console.log("Minted events: " + mintEvents.length);
+                }
+            }
+
+            lastBlockNumber = blockNumber;
+            syncFlag = false;
+            console.log("Synced events: " + lastBlockNumber + " " + events.length);
         }
-
-        console.log("filter: ", filter);
-        const events = await nftFactory.queryFilter(filter, lastBlockNumber, blockNumber);
-
-        console.log("events: ", events);
-
-        for (const event of events) {
-            nftCollectionEvents.push(event);
-            
-            const collectionAddress = event.args?.[0];
-            nftCollectionAddress.push(collectionAddress);
-
-            const collection = await ethers.getContractAt("NFTCollection", collectionAddress, provider.getSigner());
-            const mintFilter = collection.filters.TokenMinted();
-
-            nftMintFilters.push({"contract": collection, "filter": mintFilter});            
+        catch (error) {
+            console.error(error);
         }
-
-        for (const mintFilter of nftMintFilters) {
-            const mintEvents = await mintFilter.contract.queryFilter(mintFilter.filter, lastBlockNumber, blockNumber);
-            nftMintEvents.push(...mintEvents);
-        }
-        
-        lastBlockNumber = blockNumber + 1;
-        console.log("Synced events: " + lastBlockNumber + " " + events);
     }, 2000);
 }
 
